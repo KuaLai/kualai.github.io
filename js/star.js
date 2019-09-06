478c9c844ac7accd716d44c7fe8d2a346c7d5fb9
Kua <35482811+KuaLai@users.noreply.github.com>
2019-09-06 11:02:10 +0800

    Feat: 修改星星載入順序

--- js/star.js
+++ js/star.js
@@ -1,40 +1,42 @@
-var map = {
-  obj: $("#map"),
-
-  bounderies: {
-    w: $(document).width(),
-    h: $(document).height()
-  },
-
-  stars: {
-    template: $("<b></b>"),
-    total: 100,
-    star: [] //top, left, size, shine time
+window.onload = function () {
+  let map = {
+    obj: $("#map"),
+  
+    bounderies: {
+      w: $(document).width(),
+      h: $(document).height()
+    },
+  
+    stars: {
+      template: $("<b></b>"),
+      total: 100,
+      star: [] //top, left, size, shine time
+    }
+  };
+  
+  for (let index = 1; index <= map.stars.total; index++) {
+    let star = $("<b></b>"),
+      star_top = Math.round(Math.random() * map.bounderies.h + 1),
+      star_left = Math.round(Math.random() * map.bounderies.w + 1),
+      star_size = Math.round(Math.random() * 3 + 1),
+      star_shine = Math.round(Math.random() * 100) + 1,
+      star_shine_speed = "";
+  
+    star.addClass("star").css({
+      top: star_top,
+      left: star_left,
+      width: star_size,
+      height: star_size
+    });
+  
+    if (index % 3 == 0) {
+      star_shine_speed =
+        (Math.round(Math.random() * 2) + 1) % 2 == 0
+          ? "shine-fast"
+          : "shine-slow";
+      star.addClass(star_shine_speed);
+    }
+  
+    map.obj.append(star);
   }
 };
-
-for (var index = 1; index <= map.stars.total; index++) {
-  var star = $("<b></b>"),
-    star_top = Math.round(Math.random() * map.bounderies.h + 1),
-    star_left = Math.round(Math.random() * map.bounderies.w + 1),
-    star_size = Math.round(Math.random() * 3 + 1),
-    star_shine = Math.round(Math.random() * 100) + 1,
-    star_shine_speed = "";
-
-  star.addClass("star").css({
-    top: star_top,
-    left: star_left,
-    width: star_size,
-    height: star_size
-  });
-
-  if (index % 3 == 0) {
-    star_shine_speed =
-      (Math.round(Math.random() * 2) + 1) % 2 == 0
-        ? "shine-fast"
-        : "shine-slow";
-    star.addClass(star_shine_speed);
-  }
-
-  map.obj.append(star);
-}
